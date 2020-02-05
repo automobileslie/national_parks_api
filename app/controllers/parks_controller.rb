c7e51c8c86acecafb161f55fe4b63b76e1ca8152
Carlie Anglemire <carlieanglemire@Carlies-MacBook-Air.local>
2020-02-05 18:12:06 -0500

    Wednesday's second commit

--- app/controllers/parks_controller.rb
+++ app/controllers/parks_controller.rb
@@ -1,13 +1,12 @@
 class ParksController < ApplicationController
 
     def index 
-        require 'rest_client'
+        require 'rest_client'    
         nps_data= RestClient.get("https://developer.nps.gov/api/v1/parks?&API_KEY=#{ENV['NPS_API_KEY']}")
         nps_parsed= JSON.parse(nps_data)
         render json: nps_parsed
     end
-
-
+    
 end
 
 
