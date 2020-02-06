72b72cde572a424a9c3d277541b28858ea363f9f
Carlie Anglemire <carlieanglemire@Carlies-MacBook-Air.local>
2020-02-06 11:54:19 -0500

    Thursday commit

--- app/controllers/parks_controller.rb
+++ app/controllers/parks_controller.rb
@@ -1,8 +1,9 @@
 class ParksController < ApplicationController
 
     def index 
-        require 'rest_client'    
-        nps_data= RestClient.get("https://developer.nps.gov/api/v1/parks?&API_KEY=#{ENV['NPS_API_KEY']}")
+        require 'rest_client'  
+        number= params[:number]  
+        nps_data= RestClient.get("https://developer.nps.gov/api/v1/parks?&API_KEY=#{ENV['NPS_API_KEY']}&start=#{number}&limit=25")
         nps_parsed= JSON.parse(nps_data)
         render json: nps_parsed
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   resources :park_collections
   resources :users
-  resources :parks
+  # resources :parks
+
+post '/parks', to: 'parks#index'
   resources :login, only: [:create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
