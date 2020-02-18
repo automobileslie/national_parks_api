175d6c977d561829d15b39b20c8220dd6af88a41
Carlie Anglemire <carlieanglemire@Carlies-MacBook-Air.local>
2020-02-18 15:41:52 -0500

    first commit Tuesday

--- app/controllers/parks_controller.rb
+++ app/controllers/parks_controller.rb
@@ -3,7 +3,7 @@ class ParksController < ApplicationController
     def index 
         require 'rest_client'  
         number= params[:number]  
-        nps_data= RestClient.get("https://developer.nps.gov/api/v1/parks?&API_KEY=#{ENV['NPS_API_KEY']}&start=#{number}&limit=20")
+        nps_data= RestClient.get("https://developer.nps.gov/api/v1/parks?&API_KEY=#{ENV['NPS_API_KEY']}&start=#{number}&limit=100")
         nps_parsed= JSON.parse(nps_data)
         render json: nps_parsed
     end
