43fddbc116cd2c9da061b3ce309a3e1879b2366a
Carlie Anglemire <carlieanglemire@Carlies-MacBook-Air.local>
2020-03-12 17:11:37 -0400

    Thursday commit

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,18 @@ Rails.application.routes.draw do
   resources :users
   # resources :parks
 
+# post '/parks', to: 'parks#index'
+get '/parks/first_set', to: 'parks#first_fetch'
+get '/parks/second_set', to: 'parks#second_fetch'
+get '/parks/third_set', to: 'parks#third_fetch'
+get '/parks/fourth_set', to: 'parks#fourth_fetch'
+get '/parks/fifth_set', to: 'parks#fifth_fetch'
+
 post '/parks', to: 'parks#index'
-get '/parks', to: 'parks#parks'
+
+
+
+
   resources :login, only: [:create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
